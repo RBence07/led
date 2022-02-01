7c6bddcd5234d2d705d41745709fbc2c235b4eb8
RBence07 <82832084+RBence07@users.noreply.github.com>
2022-02-01 16:28:03 +0100

    Update code.js

--- code.js
+++ code.js
@@ -19,7 +19,7 @@ if (isMobile) {
 */
     
 document.getElementById("change_color").href="?r"+data;
-/*var colorPicker = new iro.ColorPicker('#picker1', {
+var colorPicker = new iro.ColorPicker('#picker1', {
     width: size,
     handleRadius: radius,
     borderWidth: 1,
@@ -71,7 +71,7 @@ var colorPicker = new iro.ColorPicker('#picker3', {
 colorPicker.on(['color:init', 'input:end'], function(color) {
     data[6] = "R3="+color.red+"G3="+color.green+"B3="+color.blue;
     document.getElementById("change_color").href="?r"+data;
-    });*/
+    });
 
 function brightness_1(){
     var value = document.getElementById("brightness_1").value;
